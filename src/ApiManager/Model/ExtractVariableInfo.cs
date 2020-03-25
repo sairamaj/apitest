656c84c1b4a77890bac46d6e0a41aeb96ce3e25c
sairamaj <sairamaj@gmail.com>
2020-03-25 15:32:34 -0700

    updated extract variable

--- src/ApiManager/Model/ExtractVariableInfo.cs
+++ src/ApiManager/Model/ExtractVariableInfo.cs
@@ -9,5 +9,23 @@
 
 		public string Variable { get; set; }
 		public string Value { get; set; }
+		public string ShortValue
+		{
+			get
+			{
+				if (this.Value == null)
+				{
+					return string.Empty;
+				}
+
+				var val = this.Value.Substring(0, this.Value.Length > 50 ? 50 : this.Value.Length);
+				if (this.Value.Length > 50)
+				{
+					val += "......";
+				}
+
+				return val;
+			}
+		}
 	}
 }
