71394ea3c47882126018333e3caf7505d68bfd19
sairamaj <sairamaj@gmail.com>
2020-04-19 15:14:04 -0700

    fixed the status fiedl

--- src/web/WebJob/ApiRunner/Model/RunEntity.cs
+++ src/web/WebJob/ApiRunner/Model/RunEntity.cs
@@ -7,6 +7,17 @@ namespace ApiRunner
     {
         public string Name {get; set;}
         public DateTime DateTime {get; set;}
+        // Enums are not saved to azure (without using some workarounds)
+        public string StatusInfo {
+            get{
+                return Status.ToString();
+            }
+            set{
+                if(Enum.TryParse(typeof(RunStatus), value,true, out var val)){
+                    this.Status = (RunStatus)val;
+                }   
+            }
+        }
         public RunStatus Status {get; set;}
         public string Message {get; set;}
     }
--- src/web/site/Models/RunEntity.cs
+++ src/web/site/Models/RunEntity.cs
@@ -12,8 +12,19 @@ namespace site.Models
         }
         
         public string Id => this.PartitionKey;
-        public string Name {get; set;}
+        public string Name => this.RowKey;
         public DateTime DateTime {get; set;}
+        // Enums are not saved to azure (without using some workarounds)
+        public string StatusInfo {
+            get{
+                return Status.ToString();
+            }
+            set{
+                if(Enum.TryParse(typeof(RunStatus), value,true, out var val)){
+                    this.Status = (RunStatus)val;
+                }   
+            }
+        }
         public RunStatus Status {get; set;}
         public string Message {get; set;}
 
