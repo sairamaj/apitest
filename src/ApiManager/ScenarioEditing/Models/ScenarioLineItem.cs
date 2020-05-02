52c393bd26df749828eea9dc18ad19543c7a25e9
sairamaj <sairamaj@gmail.com>
2020-05-02 10:21:24 -0700

    adding dynamic editing panel

--- src/ApiManager/ScenarioEditing/Models/ScenarioLineItem.cs
+++ src/ApiManager/ScenarioEditing/Models/ScenarioLineItem.cs
@@ -21,11 +21,25 @@ namespace ApiManager.ScenarioEditing.Models
 			   this.EditingModeOn = false;
 			   OnPropertyChanged(() => this.EditingModeOn);
 		   });
+
+			this.MoveUpCommand = new DelegateCommand(() => {
+				MessageBox.Show("Move up!!!");
+			});
+			this.MoveDownCommand = new DelegateCommand(() => {
+				MessageBox.Show("Move delete!!!");
+			});
+			this.DeleteCommand = new DelegateCommand(() => {
+				MessageBox.Show("delete!!!");
+			});
 		}
 
 		public string OriginalLine { get; }
 		public string Type { get; set; }
 		public ICommand StartEditingModeCommand { get; }
+		public ICommand MoveUpCommand { get; }
+		public ICommand MoveDownCommand { get; }
+		public ICommand DeleteCommand { get; }
+
 		public bool EditingModeOn { get; set; }
 		public ICommand DoneWithEditingCommand { get; }
 		public abstract string GetCommand();
