3037909e0d7f115aeccd757eab9936183313e59b
sairamaj <sairamaj@gmail.com>
2020-05-09 11:46:24 -0700

    adding views for the resource

--- src/ApiManager/Resources/ViewModels/ResourceViewModel.cs
+++ src/ApiManager/Resources/ViewModels/ResourceViewModel.cs
@@ -2,6 +2,9 @@
 using System.Diagnostics;
 using System.Windows.Input;
 using ApiManager.Resources.Model;
+using ApiManager.Resources.Views;
+using ApiManager.ScenarioEditing.CommandEditing.Views;
+using ApiManager.Utils;
 using Wpf.Util.Core.Command;
 using Wpf.Util.Core.ViewModels;
 
@@ -12,7 +15,18 @@ namespace ApiManager.Resources.ViewModels
 		public ResourceViewModel(ResourceData resourceData)
 		{
 			ResourceData = resourceData;
-			Action showDetailsAction = () => Process.Start("notepad.exe", resourceData.FileName);
+			Action showDetailsAction = () =>
+			{
+				UiHelper.SafeAction(() =>
+			   {
+				   var win = new ViewResourceWindow
+				   {
+					   DataContext = new ViewResourceViewModel(resourceData.FileName)
+				   };
+				   win.ShowDialog();
+			   }, "View");
+			};
+
 			this.EditCommandFileCommand = new DelegateCommand(showDetailsAction);
 
 		}
---
+++ src/ApiManager/Resources/ViewModels/ViewResourceViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ApiManager.Utils;
+using Wpf.Util.Core.Command;
+
+namespace ApiManager.Resources.ViewModels
+{
+	class ViewResourceViewModel
+	{
+		public ViewResourceViewModel(string fileName)
+		{
+			this.Title = fileName;
+			this.Content = File.ReadAllText(fileName);
+			this.SaveCommand = new DelegateCommand(() =>
+			{
+				UiHelper.SafeAction(() =>
+				{
+					File.WriteAllText(fileName, Content);
+					MessageBox.Show($"Saved {fileName}", "Save", MessageBoxButton.OK, MessageBoxImage.Information);
+				}
+				, "Saving");
+			});
+		}
+
+		public string Title { get; set; }
+		public ICommand SaveCommand { get; set; }
+		public string Content { get; set; }
+	}
+}
