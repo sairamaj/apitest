516725ebdc7c3e5b788acdf8e857197ed4d6282a
sairamaj <sairamaj@gmail.com>
2020-05-21 21:55:38 -0700

    added resources window

---
+++ src/ApiManager/ScenarioEditing/ScenarioCommandParser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ApiManager.ScenarioEditing.Models;
+
+namespace ApiManager.ScenarioEditing
+{
+	class ScenarioCommandParser
+	{
+		public ScenarioCommandParser(string fileName)
+		{
+			var apiScenarios = new List<ApiScenarioItem>();
+			foreach (var line in File.ReadAllLines(fileName))
+			{
+				if (new ScenarioParser().Parse(line) is ApiScenarioItem apiScenarioItem)
+				{
+					apiScenarios.Add(apiScenarioItem);
+				}
+			}
+
+			this.ApiScenarios = apiScenarios;
+		}
+
+		public IEnumerable<ApiScenarioItem> ApiScenarios { get; }
+	}
+}
---
+++ src/ApiManager/ViewModels/ScenarioResourceViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using ApiManager.Repository;
+using ApiManager.Resources.ViewModels;
+using ApiManager.Resources.Views;
+using ApiManager.ScenarioEditing.Models;
+using ApiManager.Utils;
+using Wpf.Util.Core.Command;
+using Wpf.Util.Core.ViewModels;
+
+namespace ApiManager.ViewModels
+{
+	class ScenarioResourceViewModel : CoreViewModel
+	{
+		public ScenarioResourceViewModel(ApiScenarioItem apiScenarioItem)
+		{
+			Name = apiScenarioItem.PayloadFileName;
+			this.ResourceCommand = new DelegateCommand(() =>
+			{
+				var fileName = Path.Combine(
+				ServiceLocator.Locator.Resolve<IResourceManager>().GetResourcePath(apiScenarioItem.Method),
+				apiScenarioItem.PayloadFileName);
+				UiHelper.SafeAction(() =>
+				{
+					var win = new ViewResourceWindow
+					{
+						DataContext = new ViewResourceViewModel(fileName)
+					};
+					win.ShowDialog();
+				}, "View");
+			});
+		}
+
+		public string Name { get; }
+		public ICommand ResourceCommand { get; }
+
+	}
+}
