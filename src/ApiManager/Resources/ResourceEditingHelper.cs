cd7e37ce1e85d1d5a61813f5b4692ce881f0ccd9
sairamaj <sairamaj@gmail.com>
2020-05-31 17:53:32 -0700

    adding new resource in progress

---
+++ src/ApiManager/Resources/ResourceEditingHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using ApiManager.Resources.Model;
+
+namespace ApiManager.Resources
+{
+	static class ResourceEditingHelper
+	{
+		public static ResourceData CreateNewResource(string parentPath)
+		{
+			return new ResourceData(Path.Combine(parentPath, "new.json"));
+		}
+	}
+}
--- src/ApiManager/Resources/ViewModels/ResourceContainerViewModel.cs
+++ src/ApiManager/Resources/ViewModels/ResourceContainerViewModel.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 using ApiManager.Repository;
+using Wpf.Util.Core;
+using Wpf.Util.Core.Command;
 using Wpf.Util.Core.ViewModels;
 
 namespace ApiManager.Resources.ViewModels
@@ -10,10 +15,30 @@ namespace ApiManager.Resources.ViewModels
 		public ResourceContainerViewModel(IResourceManager resourceManager, string method)
 		{
 			var resources = resourceManager.GetResources(method).ToList();
-			this.Resources = resources.Where(r => !r.IsContainer).Select(r => new ResourceViewModel(r));
-			this.Resources = this.Resources.Union(resources.Where(r => r.IsContainer).Select(r => new ResourceFolderViewModel(r)));
+			this.Resources = new SafeObservableCollection<TreeViewItemViewModel>();
+			resources
+				.Where(r => !r.IsContainer).Select(r => new ResourceViewModel(r))
+				.ToList().ForEach(r1 => this.Resources.Add(r1));
+
+			resources
+				.Where(r => r.IsContainer).Select(r => new ResourceFolderViewModel(r))
+				.ToList().ForEach(r1 => this.Resources.Add(r1));
+
+
+			this.NewFileCommand = new DelegateCommand(() => {
+				MessageBox.Show($" New item 222: {method}");
+				var newResource = ResourceEditingHelper.CreateNewResource(resourceManager.GetResourcePath(method));
+				this.Resources.Add(new ResourceViewModel(newResource));
+			});
+			this.NewFolderCommand = new DelegateCommand(() => {
+				MessageBox.Show($" New folder 333 {method}");
+			});
+
 		}
 
-		public IEnumerable<TreeViewItemViewModel> Resources { get; set; }
+		public ObservableCollection<TreeViewItemViewModel> Resources { get; set; }
+		public ICommand NewFileCommand { get; set; }
+		public ICommand NewFolderCommand { get; set; }
+
 	}
 }
